e59a97694684d328d65b8aa7634b17840c92e2dc
sajjadxdev <sajjadxdev@gmail.com>
2024-07-17 15:02:54 +0500

    first commit

---
+++ myapps/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from django.forms import ModelForm
+
+
+from .models import *
+from .forms import *
+
+class TaskForm(forms.ModelForm):
+    title=forms.CharField(widget= forms.TextInput(attrs={'placeholder':'Add new Task...'}))
+
+    class Meta:
+        model=Task
+        fields='__all__'
